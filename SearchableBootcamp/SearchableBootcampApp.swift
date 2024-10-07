2f483d7dae73f047f77a6e355d9acbd23a4148ca
Serhan Khan <serhankhan0@gmail.com>
2024-10-07 12:35:13 +0300

    Initial Commit

---
+++ SearchableBootcamp/SearchableBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchableBootcampApp.swift
+//  SearchableBootcamp
+//
+//  Created by Serhan Khan on 07/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SearchableBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
